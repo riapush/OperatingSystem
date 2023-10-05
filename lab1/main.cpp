5e234566200cce9fa234a51e8d6560c182283e24
riapush <leriya022@gmail.com>
2023-10-05 16:44:59 +0300

    added folder logging

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,8 +2,9 @@
 #include <fstream>
 #include <chrono>
 #include <thread>
+#include <filesystem>
 
-const int DEFAULT_INTERVAL = 60;
+const int DEFAULT_INTERVAL = 3;
 
 class Daemon {
 private:
@@ -25,12 +26,15 @@ public:
     void start() {
         // Daemon logic to periodically perform actions
         while (true) {
-            std::ofstream logfile("hist.log", std::ios::app);
+            std::ofstream logfile("D:/Git/GitHub/OperatingSystem/lab1/folder2/hist.log", std::ios::app);
             if (logfile.is_open()) {
+                std::filesystem::path folder1("D:/Git/GitHub/OperatingSystem/lab1/folder1");
                 auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
                 logfile << "Time: " << std::ctime(&now);
                 logfile << "Directory contents: \n";
-                // TO DO: log directory contents
+                for (const auto& entry : std::filesystem::directory_iterator(folder1)) {
+                    logfile << entry.path() << "\n";
+                }
                 logfile.close();
             } else {
                 std::cout << "Unable to open hist.log for writing." << std::endl;
